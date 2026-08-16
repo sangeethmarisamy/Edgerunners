3f88dccd0f605e8d85b9d289e86a24a0e1265004
sangeethmarisamy <kit28.24bcb046@gmail.com>
2026-08-16 10:49:56 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/buildathon/academic/AcademicIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.buildathon.academic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicIntelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildathon/academic/AcademicIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildathon.academic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
